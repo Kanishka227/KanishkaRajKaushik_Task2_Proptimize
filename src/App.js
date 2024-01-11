466c863ebcb42d1ceebb64af26bc9c40ca3abcde
Kanishka Raj Kaushik <kanishkaraj227@gmail.com>
2024-01-11 09:47:50 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { user , loginWithRedirect } = useAuth0();
+  console.log("Current Uset", user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">Welcome to Proptimize Login Page</div>
+
+      <div>
+          <button className="button-el" onClick={e => loginWithRedirect()}>Login</button>
+      </div>
     </div>
   );
 }
